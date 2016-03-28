2acfa322562dae6aa538b473edaf5e83e9d93639
William Penha <penhadr12@users.noreply.github.com>
2016-03-28 15:36:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-	// declaracao de variaveis
-   float num;
+    // declaracao de variaveis
+    float num;
 
-	// exibicao no console da pergunta do numero desejado
+    // exibicao no console da pergunta do numero desejado
     printf("Digite o numero, que queres saber\n");
     // atribuicao da resposta na variavel declarada 
     scanf("%f", &num);
 
-	// verificacao se o numero e positivo, negativo e nulo
+    // verificacao se o numero e positivo, negativo e nulo
     if(num < 0){
         printf("O numero e negativo");
     }else if(num > 0){
